fa56680e936bca2fc650c36d313896c1cab2a798
danny-smirnov <danu90_2016@mail.ru>
2023-04-03 22:10:18 +0300

    simple integrals

--- numeric/integrating/rectangular.py
+++ numeric/integrating/rectangular.py
@@ -1,9 +1,9 @@
 from typing import Callable
 import math
+import matplotlib.pyplot as plt
 
 
-def left_rectangles(func: Callable, a: float, b: float, eps=1e-4):
-    N = 100
+def left_rectangles(func: Callable, a: float, b: float, N=100):
     h = (b - a) / N
 
     integral_sum = 0
@@ -13,21 +13,50 @@ def left_rectangles(func: Callable, a: float, b: float, eps=1e-4):
     return integral_sum
 
 
-def right_rectangles(func: Callable, a: float, b: float, eps=1e-4):
-    N = 100
+def right_rectangles(func: Callable, a: float, b: float, N=100):
     h = (b - a) / N
 
     integral_sum = 0
     for i in range(N):
-        integral_sum += func(a + h * (i+1)) * h
+        integral_sum += func(a + h * (i + 1)) * h
 
     return integral_sum
 
 
+def mid_rectangles(func: Callable, a: float, b: float, N=100):
+    h = (b - a) / N
+
+    integral_sum = 0
+    for i in range(N):
+        integral_sum += func(a + h * (i + 1 / 2)) * h
+
+    return integral_sum
 
 
-if __name__=='__main__':
+def trap_rectangles(func: Callable, a: float, b: float, N=100):
+    h = (b - a) / N
 
+    integral_sum = 0
+    for i in range(N):
+        integral_sum += (func(a + h * i) + func(a + h * (i + 1))) / 2 * h
+
+    return integral_sum
+
+
+if __name__ == '__main__':
     f = lambda x: math.cos(x)
-    print(math.pi/2)
-    print(left_rectangles(f, 0, math.pi/2))
+    res = {'left':[],
+           'right':[],
+           'mid':[],
+           'trap':[]}
+    for n in range(10, 10000):
+        res['left'].append(abs(1 - left_rectangles(f, 0, math.pi/2, N=n)))
+        res['right'].append(abs(1 - right_rectangles(f, 0, math.pi/2, N=n)))
+        res['mid'].append(abs(1 - mid_rectangles(f, 0, math.pi/2, N=n)))
+        res['trap'].append(abs(1 - trap_rectangles(f, 0, math.pi/2, N=n)))
+
+
+    for key in res:
+        plt.plot(res[key], label=key)
+    plt.legend()
+    plt.show()
